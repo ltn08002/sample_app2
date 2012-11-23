1cb75fbf597bf265f55f5638f977362d92d5a909
Linh Nhan <linh.nhan@uconn.edu>
2012-11-23 17:32:55 -0500

    finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ SampleApp::Application.routes.draw do
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
+  match '/profile', to: 'users#edit'
+  match '/users',	to: 'users#index'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
